9c507c7e3c675118b27dbb930206136d30ed562c
Edward Fernando <edward.fer@gmail.com>
2014-08-26 15:46:30 +0700

    Direct call to Service instead of DAO

--- src/main/java/com/allegro/api/controller/PageController.java
+++ src/main/java/com/allegro/api/controller/PageController.java
@@ -2,9 +2,6 @@ package com.allegro.api.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,46 +11,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.allegro.api.dao.PageDAO;
 import com.allegro.api.model.Page;
+import com.allegro.api.service.PageService;
 
 @Controller
 public class PageController extends com.allegro.api.controller.Controller {
 
 	@Autowired
-	private PageDAO dao;
+	private PageService service;
 
 	@RequestMapping(value = "/pages", method = RequestMethod.GET)
 	public ResponseEntity<List<Page>> getAll() {
-		return new ResponseEntity<List<Page>>(dao.getAll(), HttpStatus.OK);
+		return new ResponseEntity<List<Page>>(service.getAll(), HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/page/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Page> get(@PathVariable("id") String id) {
-		return new ResponseEntity<Page>(dao.get(id), HttpStatus.OK);
+		return new ResponseEntity<Page>(service.get(id), HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/page", method = RequestMethod.POST)
-	public ResponseEntity<Page> create(@RequestBody Page page, HttpServletRequest request) {
-		String action = request.getParameter("action");
-
-		if (StringUtils.equals(action, "new_thread")) {
-
-		}
-
-		dao.save(page);
+	public ResponseEntity<Page> create(@RequestBody Page page) {
+		service.save(page);
 		return new ResponseEntity<Page>(HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/page", method = RequestMethod.PUT)
 	public ResponseEntity<Page> update(@RequestBody Page page) {
-		dao.update(page);
+		service.update(page);
 		return new ResponseEntity<Page>(HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/page", method = RequestMethod.DELETE)
 	public ResponseEntity<Page> delete(@RequestBody Page page) {
-		dao.delete(page);
+		service.delete(page);
 		return new ResponseEntity<Page>(HttpStatus.OK);
 	}
 
