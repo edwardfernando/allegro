2f86f2001611c43580da4e4d6908b59725c037c0
Edward Fernando <edward.fer@gmail.com>
2014-08-26 18:37:21 +0700

    Add comming criteria method

--- src/main/java/com/allegro/api/dao/AbstractDAO.java
+++ src/main/java/com/allegro/api/dao/AbstractDAO.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import com.allegro.api.model.Model;
 
@@ -14,7 +16,7 @@ public abstract class AbstractDAO<T extends Model> {
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired
-	private MongoTemplate template;
+	protected MongoTemplate template;
 
 	public boolean tableExists() {
 		return template.collectionExists(getDomain());
@@ -38,6 +40,24 @@ public abstract class AbstractDAO<T extends Model> {
 
 	public T get(String id) {
 		return template.findById(id, getDomain());
+	}
+
+	public T execUnique(String criteria, Object expectedValue) {
+		return template.findOne(new Query(Criteria.where(criteria).is(expectedValue)), getDomain());
+	}
+
+	public T execUnique(String[] criterias, Object[] expectedValues) {
+		Criteria criteria = Criteria.where(criterias[0]).is(expectedValues[0]);
+
+		for (int i = 1; i < criterias.length; i++) {
+			criteria.and(criterias[i]).is(expectedValues[i]);
+		}
+
+		return template.findOne(new Query(criteria), getDomain());
+	}
+
+	public List<T> execList(String criteria, String expectedValue) {
+		return template.find(new Query(Criteria.where(criteria).is(expectedValue)), getDomain());
 	}
 
 	public abstract Class<T> getDomain();
