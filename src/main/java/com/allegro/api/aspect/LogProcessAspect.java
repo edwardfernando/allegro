9598d19b9cd775578daeba8688702062c6287fb4
Yagi Anggar Prahara <yagi@wego.com>
2014-09-09 19:01:59 +0700

    Retrieved param passed from annotation

--- src/main/java/com/allegro/api/aspect/LogProcessAspect.java
+++ src/main/java/com/allegro/api/aspect/LogProcessAspect.java
@@ -2,12 +2,19 @@ package com.allegro.api.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import com.allegro.api.annotation.LogProcess;
+import com.allegro.api.model.Model;
 
 @Aspect
+@Component
 public class LogProcessAspect {
 
-	@Before("execution(* com.allegro.api.service.UserService.save(..))")
-	public void testLog() {
-		System.out.println("Logging TEst is running");
+	//@Before("execution(* com.allegro.api.service.UserService.save(..))")
+	@Before("@annotation(logProcess) && args(object,..)")
+	public void testLog(LogProcess logProcess, Model object) {
+		System.out.println("Logging test is running");
+		System.out.println("Context : "+object);
 	}
 }
