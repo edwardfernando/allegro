22c91020426bbe995fa57121b17c71308fd3b5de
Yagi Anggar Prahara <yagi@wego.com>
2014-09-09 19:01:31 +0700

    Added className

--- src/main/java/com/allegro/api/annotation/LogProcess.java
+++ src/main/java/com/allegro/api/annotation/LogProcess.java
@@ -5,8 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.allegro.api.model.Model;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface LogProcess {
-
+	public Class<? extends Model> className();
+	//public String className();
 }
