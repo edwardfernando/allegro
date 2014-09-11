064a530bffe33f0db9a64b9b8291e90413cbfe23
Yagi Anggar Prahara <yagi@wego.com>
2014-09-11 18:09:45 +0700

    Modified logger

--- src/main/java/com/allegro/api/aspect/LogProcessAspect.java
+++ src/main/java/com/allegro/api/aspect/LogProcessAspect.java
@@ -2,6 +2,8 @@ package com.allegro.api.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.allegro.api.annotation.LogProcess;
@@ -10,11 +12,11 @@ import com.allegro.api.model.Model;
 @Aspect
 @Component
 public class LogProcessAspect {
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 	//@Before("execution(* com.allegro.api.service.UserService.save(..))")
 	@Before("@annotation(logProcess) && args(object,..)")
 	public void testLog(LogProcess logProcess, Model object) {
-		System.out.println("Logging test is running");
-		System.out.println("Context : "+object);
+		logger.info(object.toString());
 	}
 }
