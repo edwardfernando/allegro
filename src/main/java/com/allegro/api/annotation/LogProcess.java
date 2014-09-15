519a060d5721b1028f6ff181aff54c7ed376c239
Yagi Anggar Prahara <yagi@wego.com>
2014-09-15 20:00:13 +0700

    Added actionType & detailProcess

--- src/main/java/com/allegro/api/annotation/LogProcess.java
+++ src/main/java/com/allegro/api/annotation/LogProcess.java
@@ -5,11 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.allegro.api.enumclass.ActionType;
 import com.allegro.api.model.Model;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface LogProcess {
 	public Class<? extends Model> className();
-	//public String className();
+	public ActionType actionType();
+	public String detailProcess() default "";
 }
