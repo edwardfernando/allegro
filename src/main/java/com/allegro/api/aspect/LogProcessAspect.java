1cc38748c4bae5c88c6658dd79f8d722598ae6cb
Yagi Anggar Prahara <yagi@wego.com>
2014-09-16 19:51:38 +0700

    Trying to generate log file but still failed :(

--- src/main/java/com/allegro/api/aspect/LogProcessAspect.java
+++ src/main/java/com/allegro/api/aspect/LogProcessAspect.java
@@ -1,24 +1,44 @@
 package com.allegro.api.aspect;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.allegro.api.annotation.LogProcess;
 import com.allegro.api.model.Model;
+//import org.slf4j.Logger;
 
 @Aspect
 @Component
 public class LogProcessAspect {
-	protected final Logger logger = LoggerFactory.getLogger(getClass());
+	//protected final Logger logger = LoggerFactory.getLogger(getClass());
+	private final Logger log = Logger.getLogger("ProcessLog");
+	private FileHandler fh;
 
 	@Before("@annotation(logProcess) && args(object,..)")
 	public void testLog(LogProcess logProcess, Model object) {
-		logger.info(object.toString());
-		logger.debug("Action Type : {}",logProcess.actionType());
-		logger.debug("Message : {}",logProcess.detailProcess());
+		//logger.info(object.toString());
+		//logger.debug("Action Type : {}",logProcess.actionType());
+		//logger.debug("Message : {}",logProcess.detailProcess());
+
+		try {
+			fh = new FileHandler("/var/apps/allegro/logs/process-logging.log");
+			log.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+
+			log.info("Test logggg :P");
+
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
